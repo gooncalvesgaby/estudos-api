67b3093e6189f9a9de3db75d438c737c857da915
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-12-16 00:34:04 -0300

    consumindo api com metodo fetch

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('testando')
+// o método fetch me permite realizar requisições HTTP de
+// forma ASSÍNCRONA em APIs. fetch() é uma função, e o primeiro
+// parâmetro da minha função fetch é a URL que eu quero acessar
+
+async function obtendoDados() {
+    try{
+        const resposta = await fetch('https://viacep.com.br/ws/02017010/json/')
+        const dados = await resposta.json()
+        console.log(dados)
+    } catch(error) {
+        console.log('erro na requisição', error)
+
+    } finally {
+        console.log('requisição finalizada')
+    }
+    }
+
+obtendoDados()
